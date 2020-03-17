e2e25c6cc4a3ff810ffd628005ea26857c0363b2
gmediaiosdev2 <maulana.husni@gmedia.co.id>
2020-03-17 10:11:33 +0700

    maul

---
+++ app/src/main/java/id/net/gmedia/pal/Adapter/NotaGiroAdapter.java
@@ -0,0 +1,84 @@
+package id.net.gmedia.pal.Adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.leonardus.irfan.Converter;
+
+import java.util.List;
+
+import id.net.gmedia.pal.Activity.SetoranSales;
+import id.net.gmedia.pal.Model.SetoranModel;
+import id.net.gmedia.pal.R;
+
+public class NotaGiroAdapter extends RecyclerView.Adapter<NotaGiroAdapter.NotaGiroViewHolder> {
+
+    private List<SetoranModel> listSetoran;
+    private Activity activity;
+
+    public NotaGiroAdapter(List<SetoranModel> listSetoran, Activity activity){
+        this.activity = activity;
+        this.listSetoran = listSetoran;
+    }
+
+    @NonNull
+    @Override
+    public NotaGiroViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new NotaGiroViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_setoran_sales, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotaGiroViewHolder setoranSalesViewHolder, final int i) {
+        final SetoranModel setoran = listSetoran.get(i);
+        final CheckBox cb_setoran = setoranSalesViewHolder.cb_setoran;
+
+        setoranSalesViewHolder.txt_tanggal.setText(setoran.getTanggal());
+        setoranSalesViewHolder.txt_customer.setText(setoran.getCustomer());
+        setoranSalesViewHolder.txt_nota.setText(setoran.getNota());
+        setoranSalesViewHolder.txt_jumlah.setText(Converter.doubleToRupiah(setoran.getJumlah()));
+        setoranSalesViewHolder.txt_pembayaran.setText(setoran.getPembayaran());
+
+        if (setoran.isSelected()) {
+            setoranSalesViewHolder.cb_setoran.setChecked(true);
+        }else {
+            setoranSalesViewHolder.cb_setoran.setChecked(false);
+        }
+
+        setoranSalesViewHolder.cb_setoran.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                listSetoran.get(i).setSelected(isChecked);
+                //((SetoranSales)activity).updateJumlah();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listSetoran.size();
+    }
+
+    class NotaGiroViewHolder extends RecyclerView.ViewHolder{
+
+        TextView txt_customer, txt_nota, txt_jumlah, txt_tanggal, txt_pembayaran;
+        CheckBox  cb_setoran;
+
+        NotaGiroViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txt_customer = itemView.findViewById(R.id.txt_customer);
+            txt_nota = itemView.findViewById(R.id.txt_nota);
+            cb_setoran = itemView.findViewById(R.id.cb_piutang);
+            txt_jumlah = itemView.findViewById(R.id.txt_jumlah);
+            txt_tanggal = itemView.findViewById(R.id.txt_tanggal);
+            txt_pembayaran = itemView.findViewById(R.id.txt_pembayaran);
+        }
+    }
+}
